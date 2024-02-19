9f5c9a358fa2b5b6e53aba9387539ba525c5b283
Antonio Obregon <antonio.obregon@utict.unam.mx>
2024-02-19 13:51:26 -0600

    Agregue un ejercicio un cliente para probar el issue de CORS

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,4 +1,5 @@
 const express = require('express') // require -> commonJS
+
 const crypto = require('node:crypto')
 const cors = require('cors')
 
@@ -6,34 +7,13 @@ const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
-app.use(express.json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8080',
-      'http://localhost:1234',
-      'https://movies.com',
-      'https://midu.dev'
-    ]
-
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
+app.use(express.json())
 
-// CORS PRE-Flight
-// OPTIONS
+app.get('/', (req, res) => {
+  res.json({message: 'hola mundo'})
+})
 
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
@@ -47,12 +27,14 @@ app.get('/movies', (req, res) => {
   res.json(movies)
 })
 
-app.get('/movies/:id', (req, res) => {
+app.get('/movies/:id', (req, res) => { //path-to-regexp
   const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
+  
   res.status(404).json({ message: 'Movie not found' })
 })
+
 
 app.post('/movies', (req, res) => {
   const result = validateMovie(req.body)
@@ -75,19 +57,8 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie)
 })
 
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
 
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1)
-
-  return res.json({ message: 'Movie deleted' })
-})
-
+// Actualizar una pelicuala
 app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body)
 
@@ -112,6 +83,54 @@ app.patch('/movies/:id', (req, res) => {
   return res.json(updateMovie)
 })
 
+/*
+
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'x
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+
+
+
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+*/
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
